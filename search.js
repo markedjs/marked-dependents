0ccb5c1771ac542e4711135e8bda61ea8c25f39c
Tony Brix <tony@brix.ninja>
2018-04-13 01:08:38 -0500

    add dlGt1000

--- search.js
+++ search.js
@@ -4,15 +4,24 @@ const packages = fs.readdirSync("./packages");
 
 
 let versions = {};
+let dlGt1000 = [];
 packages.forEach((file) => {
 	const pkg = require(`./packages/${file}`);
+
+	// versions
 	const ver = pkg.depVersion;
 	if (!versions[ver]) {
 		versions[ver] = 0;
 	}
-
 	versions[ver]++;
+
+	// dlGt1000
+	if (pkg.downloadsLastWeek > 1000) {
+		dlGt1000.push(pkg.name);
+	}
 });
 
 versions = Object.entries(versions).sort((a, b) => (b[1] - a[1])).map(a => a.reverse());
 fs.writeFileSync("./stats/versions.json", JSON.stringify(versions, null, "\t"));
+
+fs.writeFileSync("./stats/dlGt1000.json", JSON.stringify(dlGt1000, null, "\t"));
