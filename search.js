4155f83bdafb786dbcf49e9c25455a486e9266f3
Tony Brix <tony@brix.ninja>
2018-04-13 01:14:34 -0500

    add releaseThisYear

--- search.js
+++ search.js
@@ -6,6 +6,7 @@ const packages = fs.readdirSync("./packages");
 let versions = {};
 const dlGt1000 = [];
 const modThisYear = [];
+const releaseThisYear = [];
 
 packages.forEach((file) => {
 	const pkg = require(`./packages/${file}`);
@@ -26,11 +27,16 @@ packages.forEach((file) => {
 	if (new Date(pkg.modified) > new Date("2018-01-01")) {
 		modThisYear.push(pkg.name);
 	}
+
+	// releaseThisYear
+	if (new Date(pkg.released) > new Date("2018-01-01")) {
+		releaseThisYear.push(pkg.name);
+	}
 });
 
 versions = Object.entries(versions).sort((a, b) => (b[1] - a[1])).map(a => a.reverse());
 fs.writeFileSync("./stats/versions.json", JSON.stringify(versions, null, "\t"));
 
 fs.writeFileSync("./stats/dlGt1000.json", JSON.stringify(dlGt1000, null, "\t"));
-
 fs.writeFileSync("./stats/modThisYear.json", JSON.stringify(modThisYear, null, "\t"));
+fs.writeFileSync("./stats/releaseThisYear.json", JSON.stringify(releaseThisYear, null, "\t"));
